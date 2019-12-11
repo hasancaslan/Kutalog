8cc96003747fd7e3a1da227519b3654537e4a963
Hasan Can <aslan4044@gmail.com>
2019-12-11 21:34:31 +0300

    Class Search Search Bar implemented

---
+++ KUtalog/Extensions/+UIImage.swift
@@ -0,0 +1,9 @@
+//
+//  +UIImage.swift
+//  KUtalog
+//
+//  Created by HASAN CAN on 11.12.2019.
+//  Copyright © 2019 cerenhasancan. All rights reserved.
+//
+
+import Foundation
--- KUtalog/View Controllers/Main Views/ClassSearchViewController.swift
+++ KUtalog/View Controllers/Main Views/ClassSearchViewController.swift
@@ -12,7 +12,7 @@ import CoreData
 class ClassSearchViewController: UIViewController {
     @IBOutlet weak var classListCollectionView: UICollectionView!
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-         private let itemsPerRow: CGFloat = 1
+    private let itemsPerRow: CGFloat = 1
     
     private lazy var dataSource: ClassSearchDataSource = {
         let source = ClassSearchDataSource()
@@ -96,7 +96,7 @@ extension ClassSearchViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        //2
+        
         let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
         let availableWidth = view.frame.width - paddingSpace
         let widthPerItem = availableWidth / itemsPerRow
@@ -104,14 +104,12 @@ extension ClassSearchViewController: UICollectionViewDelegateFlowLayout {
         return CGSize(width: widthPerItem, height: 2 * widthPerItem / 3)
     }
     
-    //3
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         insetForSectionAt section: Int) -> UIEdgeInsets {
         return sectionInsets
     }
     
-    // 4
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         minimumLineSpacingForSectionAt section: Int) -> CGFloat {
