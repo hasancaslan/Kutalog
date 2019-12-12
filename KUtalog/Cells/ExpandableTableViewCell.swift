205ae82ed0954173f813a5c2068599493e4c886c
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2019-12-12 16:09:36 +0300

    Alert added for login

---
+++ KUtalog/Cells/ExpandableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExpandableTableViewCell.swift
+//  KUtalog
+//
+//  Created by Ceren on 12.12.2019.
+//  Copyright © 2019 cerenhasancan. All rights reserved.
+//
+
+import UIKit
+
+class ExpandableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
