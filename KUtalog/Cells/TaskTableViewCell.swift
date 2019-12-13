675d5df1a248872c848647da0c9c8e0d12b27d89
Hasan Can <aslan4044@gmail.com>
2019-12-13 03:15:12 +0300

    data source connected to TasksViewController

--- KUtalog/Cells/TaskTableViewCell.swift
+++ KUtalog/Cells/TaskTableViewCell.swift
@@ -12,7 +12,10 @@ class TaskTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var courseCodeLabel: UILabel!
-    @IBOutlet weak var timeLabel: NSLayoutConstraint!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    @IBOutlet weak var editButton: UIButton!
     
     
     override func awakeFromNib() {
@@ -23,6 +26,14 @@ class TaskTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         // Configure the view for the selected state
+    }
+    
+    func configure(task: Task?) {
+        titleLabel.text = task?.title
+        courseCodeLabel.text = task?.moduleCode
+        //TODO: date format adjustment
+        timeLabel.text = "\(String(describing: task?.date))"
+        descriptionTextField.text = task?.taskDescription
     }
 
 }
--- KUtalog/View Controllers/Main Views/TasksViewController.swift
+++ KUtalog/View Controllers/Main Views/TasksViewController.swift
@@ -7,32 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
 class TasksViewController: UIViewController {
-
     @IBOutlet weak var tasksTableView: UITableView!
-    
+    let reuseIdentifier = "TaskTableViewCell"
     var selectedRowIndex = -1
     var thereIsCellTapped = false
+    var allTasks: [Task]? = [Task]()
+    
+    private lazy var dataSource: TasksDataSource = {
+        let source = TasksDataSource()
+        source.fetchedResultsControllerDelegate = self
+        source.delegate = self
+        return source
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tasksTableView.delegate = self
         self.tasksTableView.dataSource = self
-        // Do any additional setup after loading the view.
     }
     
-// MARK:- Actions
-    /*
-    // MARK: - Navigation
+    // MARK:- Helpers
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
+}
+
+// MARK:- Table View Delegate
+extension TasksViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == selectedRowIndex && thereIsCellTapped {
             return 250
@@ -57,26 +69,38 @@ class TasksViewController: UIViewController {
         }
         tasksTableView.beginUpdates()
         tasksTableView.endUpdates()
-
-
-}
-}
-
-// MARK:- Table View Delegate
-extension TasksViewController: UITableViewDelegate {
-    
+    }
 }
 
 // MARK:- Table View Data Source
 extension TasksViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return allTasks?.count ?? 0
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "ExpandableTableViewCell"){
+        if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) as? TaskTableViewCell {
+            cell.configure(task: allTasks?[indexPath.row])
             return cell
         }
         return UITableViewCell()
     }
 }
+
+// MARK: - NS Fetched Results Controller Delegate
+extension TasksViewController: NSFetchedResultsControllerDelegate {
+    /**
+     Reloads the table view when the fetched result controller's content changes.
+     */
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        
+    }
+}
+
+extension TasksViewController: TasksDataSourceDelegate {
+    func taskListLoaded(taskList: [Task]?) {
+        self.allTasks = taskList
+        self.tasksTableView.reloadData()
+    }
+}
+
