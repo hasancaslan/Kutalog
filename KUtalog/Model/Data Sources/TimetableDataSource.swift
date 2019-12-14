18054a0820f43d9f1626575a6b6b8710228dc9cd
Hasan Can <aslan4044@gmail.com>
2019-12-14 22:04:03 +0300

    timetable data source implemented

--- KUtalog/Model/Data Sources/TimetableDataSource.swift
+++ KUtalog/Model/Data Sources/TimetableDataSource.swift
@@ -14,51 +14,17 @@ protocol TimetableDataSourceDelegate {
 
 class TimetableDataSource {
     // MARK: - Core Data
-    /**
-     A persistent container to set up the Core Data stack.
-     */
     lazy var persistentContainer = DataController.shared.persistentContainer
     
-    func createTask(uid: String, title: String?, moduleCode: String?, description: String?, date: Date?, course: Course) {
-        let task = Task(context: persistentContainer.viewContext)
-        task.uid = uid
-        task.title = title
-        task.moduleCode = moduleCode
-        task.taskDescription = description
-        task.date = date
-        task.course = course
-        try? persistentContainer.viewContext.save()
-    }
-    
-    func deleteTask(_ taskToDelete: Task) {
-        persistentContainer.viewContext.delete(taskToDelete)
-        try? persistentContainer.viewContext.save()
-    }
-    
     // MARK: - NSFetchedResultsController
-    
-    /**
-     A fetched results controller delegate to give consumers a chance to update
-     the user interface when content changes.
-     */
     weak var fetchedResultsControllerDelegate: NSFetchedResultsControllerDelegate?
-    
-    /**
-     A fetched results controller to fetch Course records sorted by time.
-     */
-    lazy var fetchedResultsController: NSFetchedResultsController<Task> = {
-        
-        // Create a fetch request for the Course entity sorted by time.
-        let fetchRequest = NSFetchRequest<Task>(entityName: "Task")
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
-        
-        // Create a fetched results controller and set its fetch request, context, and delegate.
+    lazy var fetchedResultsController: NSFetchedResultsController<Schedule> = {
+        let fetchRequest = NSFetchRequest<Schedule>(entityName: "Schedule")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "uid", ascending: false)]
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest,
                                                     managedObjectContext: persistentContainer.viewContext,
-                                                    sectionNameKeyPath: nil, cacheName: nil)
+                                                    sectionNameKeyPath: nil, cacheName: "schedule")
         controller.delegate = fetchedResultsControllerDelegate
-        
-        // Perform the fetch.
         do {
             try controller.performFetch()
         } catch {
@@ -68,12 +34,16 @@ class TimetableDataSource {
         return controller
     }()
     
-    var delegate: TasksDataSourceDelegate?
-    
-    func loadListOfTasks() {
-        let fetchedObjects = fetchedResultsController.fetchedObjects
-        DispatchQueue.main.async {
-            self.delegate?.taskListLoaded(taskList: fetchedObjects)
+    var delegate: TimetableDataSourceDelegate?
+
+    func loadSchedule(uid: String) {
+        let fetchedObjects = fetchedResultsController.fetchedObjects?.filter({ schedule in
+            schedule.uid == uid
+        })
+        if let currentSchedule = fetchedObjects?.first {
+            DispatchQueue.main.async {
+                self.delegate?.scheduleLoaded(schedule: currentSchedule)
+            }
         }
     }
 }
